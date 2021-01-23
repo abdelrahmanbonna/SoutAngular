7817b6bcb0684119910eec757a21472ca3e9ab65
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2021-01-24 01:18:40 +0200

    adding components and editing others

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { HomeComponent } from './users/components/home/home.component';
 
 const routes: Routes = [
   { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: '', redirectTo: '/users/Home', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
