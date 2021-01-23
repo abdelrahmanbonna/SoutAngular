621684147dacdd08ca64bfc12b39f51dc31726f0
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2021-01-24 01:37:50 +0200

    editing routes and adding landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { HomeComponent } from './users/components/home/home.component';
 
 const routes: Routes = [
   { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: '', redirectTo: '/users/Home', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: '**', component: NotfoundComponent },
 ];
 
