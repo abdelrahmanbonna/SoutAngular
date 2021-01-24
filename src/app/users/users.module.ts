ae3c226eab3e6372784decfebf2664ae8d8d22ca
Heba Taher Mohammed <heba09211@gmail.com>
2021-01-24 12:46:07 +0200

    components added by heba

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -6,10 +6,13 @@ import { UsersComponent } from './users.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/search/search.component';
+import { NotoficationComponent } from './components/notofication/notofication.component';
+import { ReportsComponent } from './components/reports/reports.component';
 
 
 @NgModule({
-  declarations: [UsersComponent, SidebarComponent, HeaderComponent, FooterComponent],
+  declarations: [UsersComponent, SidebarComponent, HeaderComponent, FooterComponent, SearchComponent, NotoficationComponent, ReportsComponent],
   imports: [
     CommonModule,
     UsersRoutingModule
