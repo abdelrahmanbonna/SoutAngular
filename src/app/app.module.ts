468076c46c49f460966560cd1dd76ed63131054d
Fatma <fatma.gamal1998853@gmail.com>
2021-01-24 16:30:54 +0200

    fatma components changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChatComponent } from './components/chat/chat.component';
+import { BookmarkComponent } from './components/bookmark/bookmark.component';
+import { SettingComponent } from './components/setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    BookmarkComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
