729596d5f6fc6306bb319e0f648cd6af2ad33d08
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2021-01-29 23:44:31 +0200

    adding user service and connecting landing

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,18 @@
+export class User {
+    constructor(public id: string = "",
+        public firstName: string = "",
+        public secondName: string = "",
+        public gender: string = "",
+        public mobile: string = "",
+        public picURL: string = "",
+        public coverPicURL: string = "",
+        public birthDate: Date = new Date(),
+        public privateAcc: boolean = false,
+        public favColor: string = "",
+        public favMode: string = "",
+        public notifications: any[] = [],
+        public bookmarks: any[] = [],
+        public followers: any[] = [],
+        public following: any[] = [],) { }
+
+}
