e665f1a48d60135bbd3340f2f5402f29df8836a8
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2021-01-30 20:43:06 +0200

    finishing landing and home except posts

--- src/app/users/components/home/home.component.ts
+++ src/app/users/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserInfoService } from 'src/app/services/user-info.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  user: any;
+  greating: string;
+  constructor(private usrInfo: UserInfoService, private route: Router) {
+    this.user = JSON.parse(localStorage.getItem('userdata')!);
+    console.log(this.user)
+    this.greating = "What's up, " + this.user.firstName + " " + this.user.secondName + "?";
+
+  }
 
   ngOnInit(): void {
+    if (!localStorage.getItem('userdata')) {
+      this.route.navigate(['/landing'])
+    }
+    document.querySelector('.modal-backdrop')!.remove();
+
   }
 
 }
